934653f39c943d3f59493190cc24c1c06c924b70
angelargd8 <100092195+angelargd8@users.noreply.github.com>
2024-05-22 11:16:41 -0600

    storybook

---
+++ vite-project/src/tests/calculadora.stories.js
@@ -0,0 +1,46 @@
+import Calculadora from "../elementos/calculadora";
+import { fn } from '@storybook/test'
+import {action} from '@storybook/addon-actions'
+
+const meta ={
+    title: 'Calculadora',
+    component: Calculadora,
+
+}
+export default meta
+
+
+export const Default = {
+    args: {
+        operacion: '3+3',
+        onclick : action('onClick event') 
+    },
+}
+
+export const Decimal = {
+    args: {
+        operacion: '2.5+2.5', 
+        onclick : fn()
+    },
+}
+
+export const ComplexOperations = {
+    args: {
+        operacion: '2+2*2', 
+        onclick : fn()
+    },
+}
+
+export const ZeroOperations = {
+    args: {
+        operacion: '0+0', 
+        onclick : fn()
+    },
+}
+
+export const Error = {
+    args: {
+        operacion: '2+2*', 
+        onclick : fn()
+    },
+}
